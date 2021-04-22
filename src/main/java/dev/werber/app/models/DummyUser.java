31497115523868bdaeaeaef5ca33656282fa3f7a
Kurt Werber <kurtwerber@Kurts-MBP-2.fios-router.home>
2021-04-21 21:59:12 -0400

    user details implementation

---
+++ src/main/java/dev/werber/app/models/DummyUser.java
@@ -0,0 +1,55 @@
+package dev.werber.app.models;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@SuppressWarnings("serial")
+public class DummyUser implements UserDetails {
+	private final String username;
+	private final String password;
+	
+	public DummyUser(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(() -> "READ");
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
